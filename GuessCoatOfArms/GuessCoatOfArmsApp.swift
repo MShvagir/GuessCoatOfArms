9b74de778c42b0dfac0f9b76bc9913d69d2e81f6
Marina Shvagir <alira_m@mail.ru>
2022-05-16 20:16:36 +0400

    Initial Commit

---
+++ GuessCoatOfArms/GuessCoatOfArmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessCoatOfArmsApp.swift
+//  GuessCoatOfArms
+//
+//  Created by Марина Швагирь on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessCoatOfArmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
